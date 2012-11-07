4a6de05a0ebfe631140002c0ed79e502cd71e0b4
Brian McKenna <brian@precog.com>
2012-11-06 23:57:56 -0700

    Make the demo say something interesting

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ import Network.Wai
 import Network.Wai.Handler.Warp (run)
 
 application _ = return $
-  responseLBS status200 [("Content-Type", "text/plain")] "Hello World"
+  responseLBS status200 [("Content-Type", "text/plain")] "Hello world.\n\nThis is Haskell on Heroku.\nhttps://github.com/pufuwozu/haskell-buildpack-demo"
 
 main = do
   port <- getEnv "PORT"
